ece0b3685757b84a240c91fd775d27818ecdb37b
Ambrosius <ambrosius411@protonmail.com>
2023-04-23 17:05:41 +0200

    Initialize project and webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
